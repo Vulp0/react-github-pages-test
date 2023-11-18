4aaaa089b4cd4db2f5fadabfcdb3a9ecba2275a3
Mau <mauriciomartinezvazquez@rocketmail.com>
2023-11-17 18:47:24 -0600

    W

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Where the FUCK is my website?</h1>
+      <s><h1>Where the FUCK is my website?</h1></s>
+      <br/>
+      <h2>here the FUCK it is, this is why:</h2>
+      <p>The reason why it was coming up as blank is because the link in the homepage attribute (in the package.json) has to be the link to the deployment, not the link to the github repo itself </p>
     </div>
   );
 }
